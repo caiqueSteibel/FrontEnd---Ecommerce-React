f08ab0d0dfd164ccd94569d6ace559049c1fb1da
caiqueSteibel <caique.steibel@edu.pucrs.br>
2024-12-30 21:59:47 -0300

    add react-router-dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,35 @@
 import logo from '../assets/logo/hashtag.svg';
 import UserButtons from './UserButtons';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
-  return (
+  const { pathname } = useLocation();
+  const completeHeader = (
     <header className="flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-xl z-10">
-      <a href="/">
+      <Link to="/">
         <img
           className="h-16 px-2"
           src={logo}
           alt="Logo da Hashtag Treinamentos"
         />
-      </a>
+      </Link>
       <UserButtons />
     </header>
   );
+
+  const simpleHeader = (
+    <header className="flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-xl z-10">
+      <Link to="/">
+        <img
+          className="h-12 px-2"
+          src={logo}
+          alt="Logo da Hashtag Treinamentos"
+        />
+      </Link>
+    </header>
+  );
+
+  return <>{pathname === '/' ? completeHeader : simpleHeader}</>;
 };
 
 export default Header;
--- src/components/UserButtons.jsx
+++ src/components/UserButtons.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBagShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const UserButtons = () => {
   return (
@@ -13,9 +14,9 @@ const UserButtons = () => {
           3
         </div>
       </button>
-      <a className="px-2">
+      <Link to="/history" className="px-2">
         <FontAwesomeIcon icon={faUser} />
-      </a>
+      </Link>
     </div>
   );
 };
